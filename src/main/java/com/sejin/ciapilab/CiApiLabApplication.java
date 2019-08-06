bd9729944f4b423c0a78cd177f4578807defa0f4
Sejin Jeon <jinaidy93@gmail.com>
2019-08-06 11:10:01 +0900

    Make basic Spring Boot structure

---
+++ src/main/java/com/sejin/ciapilab/CiApiLabApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.ciapilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiApiLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiApiLabApplication.class, args);
+    }
+
+}
